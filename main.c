0f60674e4f6ab6486a425c195bb92471e96c5b0a
vslkvsky <138450738+vslkvsky@users.noreply.github.com>
2023-07-04 12:17:10 +0300

    lab1 code

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int t1, t2, t3; //За скільки часу кожен їсть торт
+    float t, tt;    //Додаткові змінні
+
+    printf("Enter times\n");
+    scanf("%d",&t1);
+    scanf("%d",&t2);
+    scanf("%d",&t3);
+
+    t = (t1 + t2 + t3)/3;
+    tt = t/3;
+
+    printf("Час необхідний для з'їдання пирога: %.2f годин",tt);
+
+
+    return 0;
+}
