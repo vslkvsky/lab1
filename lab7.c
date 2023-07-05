ec48827b3336f0349b2bd244ef2098dd1a8f8934
vslkvsky <138450738+vslkvsky@users.noreply.github.com>
2023-07-05 12:08:01 +0300

    lab7

---
+++ lab7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x1, y1, r1, x2, y2, r2;
+    float length;
+    printf("Введіть координати центра першого кола:\n");
+    scanf("%d",&x1);
+    scanf("%d",&y1);
+    printf("Введіть радіус першого кола:\n");
+    scanf("%d",&r1);
+    printf("Введіть координати центра другого кола:\n");
+    scanf("%d",&x2);
+    scanf("%d",&y2);
+    printf("Введіть радіус другого кола:\n");
+    scanf("%d",&r2);
+    length = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));   //обчислення по формулі
+    if(abs(r1 - r2) < length < (r1 + r2)) printf("Кількість точок перетину: 2\n");
+    else if(abs(r1 - r2) <= length <= (r1 + r2)) printf("Кількість точок перетину: 1\n");
+    else printf("Кількість точок перетину: 0\n");
+    
+    return 0;
+}
